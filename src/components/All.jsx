69dccdb65648bf334a6d9046cc324203a91d5685
GachiKing <bositxamdamov@gmail.com>
2023-02-14 18:11:05 +0500

    child

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
-export const All = () => {
+export const All = ({children}) => {
   return (
-    <div>All</div>
+    <div>
+      <Header/>
+      {children}
+      <Footer/>
+    </div>
   )
 }
+
+export default All;
