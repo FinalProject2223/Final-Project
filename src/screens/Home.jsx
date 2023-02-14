d29a7ae8a54b048f2549bbb46f019c9d474b42cf
GachiKing <bositxamdamov@gmail.com>
2023-02-14 21:30:47 +0500

    pushing

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import All from '../components/All';
+import FirstPart from '../components/FisrtPart/FirstPart';
 
 const Home = () => {
   return (
     <All>
-        
+        <FirstPart/>
     </All>
   )
 }
