913ce80acc836611cf3665000828621182f269d0
MustafaPolatov <mustafapolatov18@gmail.com>
2023-02-15 21:11:47 +0500

    dtfghbjnk

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import SecondPart from "./components/SecondPart/SecondPart";
 import "./index.css";
 import Home from "./screens/Home";
 import All from './components/All';
-import { Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
+import Course from "./screens/Course";
 
 function App() {
   return (
     <div className="App">
       <All>
-
+     
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Course />} />
+      </Routes>
+     
       </All>
     </div>
   );
--- src/components/All.jsx
+++ src/components/All.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../screens/Home";
 import Course from "../screens/Course";
 
@@ -9,10 +9,7 @@ export const All = ({ children }) => {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/course" element={<Course />} />
-      </Routes>
+     
       {children}
       <Footer />
     </div>
