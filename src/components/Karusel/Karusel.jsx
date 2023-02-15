6e206be649f67b5441d53cbfbfa8d0d3651e7a7b
MustafaPolatov <mustafapolatov18@gmail.com>
2023-02-15 23:25:29 +0500

    rtdfyg

--- src/components/Karusel/Karusel.jsx
+++ src/components/Karusel/Karusel.jsx
@@ -8,7 +8,7 @@ export const Karusel = () => {
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     speed: 2000,
@@ -21,7 +21,7 @@ export const Karusel = () => {
    <div className='ortaga_opchiq'>
      <Slider {...settings} className="slider_width">
           <div className='slider_img_box'>
-            <img src="https://yt3.googleusercontent.com/kqQRPQPl1c-EmKH4lgJJqtmtT4WT3IPcbzHcyfOJhTuCdq-N0oCTllYBcse-JG1O4R18y7_2=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
+            <img src="https://cdn.dribbble.com/users/2998321/screenshots/16243707/frame_85_4x.jpg" alt="" className='img_karusel' />
           </div>
           <div className='slider_img_box'>
             <img src="https://yt3.googleusercontent.com/5zbMq2YIFxg6mfQZ2WW-v5jk_zeY-HqqQ3rwwzGg9DIZMpz7U2VYG-w7p7FQVlCncEykgsSkkw=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
@@ -30,10 +30,10 @@ export const Karusel = () => {
             <img src="https://www.spot.uz/media/img/2019/05/QU2AG715592005873613_b.jpg" alt="" className='img_karusel' />
           </div>
           <div className='slider_img_box'>
-            <img src="https://yt3.googleusercontent.com/kqQRPQPl1c-EmKH4lgJJqtmtT4WT3IPcbzHcyfOJhTuCdq-N0oCTllYBcse-JG1O4R18y7_2=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
+            <img src="https://cdn.dribbble.com/users/2998321/screenshots/16243707/frame_85_4x.jpg" alt="" className='img_karusel' />
           </div>
           <div className='slider_img_box'>
-            <img src="https://yt3.googleusercontent.com/kqQRPQPl1c-EmKH4lgJJqtmtT4WT3IPcbzHcyfOJhTuCdq-N0oCTllYBcse-JG1O4R18y7_2=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
+            <img src="https://kursi24.uz/upload/iblock/3fe/3fed21cae9c2e2a1cfd173f40697379d.jpg" alt="" className='img_karusel' />
           </div>
          
          
