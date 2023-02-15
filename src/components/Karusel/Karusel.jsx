32bba5f1561543beaf1f357241e87fcc9a19b170
GachiKing <bositxamdamov@gmail.com>
2023-02-15 22:53:57 +0500

    ad

--- src/components/Karusel/Karusel.jsx
+++ src/components/Karusel/Karusel.jsx
@@ -12,27 +12,27 @@ export const Karusel = () => {
     slidesToScroll: 1,
     autoplay: true,
     speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear"
+    autoplaySpeed: 4000,
+    cssEase: "none"
   };
 
 
   return (
    <div className='ortaga_opchiq'>
      <Slider {...settings} className="slider_width">
-          <div className='bg-success'>
+          <div className='slider_img_box'>
             <img src="https://yt3.googleusercontent.com/kqQRPQPl1c-EmKH4lgJJqtmtT4WT3IPcbzHcyfOJhTuCdq-N0oCTllYBcse-JG1O4R18y7_2=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
           </div>
-          <div className='bg-success'>
+          <div className='slider_img_box'>
             <img src="https://yt3.googleusercontent.com/5zbMq2YIFxg6mfQZ2WW-v5jk_zeY-HqqQ3rwwzGg9DIZMpz7U2VYG-w7p7FQVlCncEykgsSkkw=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
           </div>
-          <div className='bg-success'>
+          <div className='slider_img_box'>
             <img src="https://www.spot.uz/media/img/2019/05/QU2AG715592005873613_b.jpg" alt="" className='img_karusel' />
           </div>
-          <div className='bg-success'>
+          <div className='slider_img_box'>
             <img src="https://yt3.googleusercontent.com/kqQRPQPl1c-EmKH4lgJJqtmtT4WT3IPcbzHcyfOJhTuCdq-N0oCTllYBcse-JG1O4R18y7_2=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
           </div>
-          <div className='bg-success'>
+          <div className='slider_img_box'>
             <img src="https://yt3.googleusercontent.com/kqQRPQPl1c-EmKH4lgJJqtmtT4WT3IPcbzHcyfOJhTuCdq-N0oCTllYBcse-JG1O4R18y7_2=s900-c-k-c0x00ffffff-no-rj" alt="" className='img_karusel' />
           </div>
          
